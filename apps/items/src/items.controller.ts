064053d5ac479c98569fda362e33e07b0658fe46
Alykam Budzaki <alykam@meucrediario.com.br>
2021-06-18 14:27:36 -0300

    ajustes cli

--- apps/items/src/items.controller.ts
+++ apps/items/src/items.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { ItemsService } from './items.service';
+import { Controller, Get } from '@nestjs/common'
+
+import { ItemsService } from './items.service'
 
 @Controller()
 export class ItemsController {
-  constructor(private readonly itemsService: ItemsService) {}
+    constructor(private readonly itemsService: ItemsService) {}
 
-  @Get()
-  getHello(): string {
-    return this.itemsService.getHello();
-  }
+    @Get()
+    public getHello(): string {
+        return this.itemsService.getHello()
+    }
 }
