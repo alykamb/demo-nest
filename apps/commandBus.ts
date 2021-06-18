5b3bbfb8947b725f80cd93ca1f88db79fd8c086a
Alykam Budzaki <alykam@meucrediario.com.br>
2021-06-18 14:49:23 -0300

    add commandbus

---
+++ apps/commandBus.ts
@@ -0,0 +1,30 @@
+import { setTimeout } from 'timers/promises'
+
+type Call = (...args: any[]) => any | Promise<any>
+
+class CommandBus {
+    private handlers = new Map<string, Call>()
+    public registerHandler(type: string, call: Call): void {
+        this.handlers.set(type, call)
+    }
+
+    public async execute(type: string, payload: any): Promise<any> {
+        const call = this.handlers.get(type)
+        if (!call) {
+            throw new Error('handler não existe')
+        }
+        return call(payload)
+    }
+}
+
+const commandBus = new CommandBus()
+
+function createThing(): any {
+    return setTimeout(2000).then(() => true)
+}
+
+commandBus.registerHandler('createThing', createThing)
+commandBus.registerHandler('somaUm', (n: number) => n + 1)
+
+void commandBus.execute('createThing', 1).then(console.log)
+void commandBus.execute('somaUm', 2).then(console.log)
