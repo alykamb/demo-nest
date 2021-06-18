5edf1b095e763ec9af3dd9c0c330d4cc72099ef0
matheusduarte <matheusduarte@t2.dev.br>
2021-06-18 16:33:48 -0300

    monsters

--- apps/commandBus.ts
+++ apps/commandBus.ts
@@ -1,13 +1,21 @@
 import { Inject, Injectable } from '@nestjs/common'
 
+import { BullMq } from './bullMq.transport'
+
 type Call = (...args: any[]) => any | Promise<any>
+type Payload = {
+    type: string
+    payload?: any
+}
 
 export const NAME = Symbol()
 
 @Injectable()
 export class CommandBus {
     private handlers = new Map<string, Call>()
-    constructor(@Inject(NAME) public name: string) {}
+    constructor(@Inject(NAME) public name: string, public bullmq: BullMq) {
+        this.bullmq.createWorker(name, ({ type, payload }: Payload) => this.execute(type, payload))
+    }
 
     public registerHandler(type: string, call: Call): void {
         this.handlers.set(type, call)
