52909bbe3850a1ad83fa371d0cbde836d809a74c
Alykam Budzaki <alykam@meucrediario.com.br>
2021-06-18 16:35:03 -0300

    commandBus

--- apps/bullMq.transport.ts
+++ apps/bullMq.transport.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable, OnModuleDestroy } from '@nestjs/common'
-import { Worker } from 'bullmq'
+import { Queue, QueueEvents, Worker } from 'bullmq'
 import Redis from 'ioredis'
 
 import { REDIS, RedisAddress } from './redis'
@@ -8,11 +8,23 @@ type GenericFunction = (...args: any[]) => any | Promise<any>
 @Injectable()
 export class BullMq implements OnModuleDestroy {
     private worker: Worker
+    private queues = new Map<string, Queue>()
+    private queuesEvents = new Map<string, QueueEvents>()
     private redis: Redis.Redis
 
     constructor(@Inject(REDIS) public redisAddress: RedisAddress) {
         this.redis = new Redis(redisAddress)
     }
+
+    private createQueue(name: string): void {
+        if (this.queues.has(name)) {
+            return
+        }
+        this.queues.set(name, new Queue(name))
+        this.queuesEvents.set(name, new QueueEvents(name))
+    }
+
+    // public addJob()
 
     public createWorker(name: string, callback: GenericFunction): void {
         this.worker = new Worker(name, callback, { connection: this.redis })
