39c9c13a9092c1e14ee996cf1aa80bedb1420dea
Alykam Budzaki <alykam@meucrediario.com.br>
2021-06-18 16:42:31 -0300

    create queue

--- apps/bullMq.transport.ts
+++ apps/bullMq.transport.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable, OnModuleDestroy } from '@nestjs/common'
-import { Queue, QueueEvents, Worker } from 'bullmq'
+import { Job, Queue, QueueEvents, Worker } from 'bullmq'
 import Redis from 'ioredis'
+import { Subject } from 'rxjs'
 
 import { REDIS, RedisAddress } from './redis'
 type GenericFunction = (...args: any[]) => any | Promise<any>
@@ -10,21 +11,35 @@ export class BullMq implements OnModuleDestroy {
     private worker: Worker
     private queues = new Map<string, Queue>()
     private queuesEvents = new Map<string, QueueEvents>()
+
+    private responses$ = new Subject<Job>()
     private redis: Redis.Redis
 
     constructor(@Inject(REDIS) public redisAddress: RedisAddress) {
         this.redis = new Redis(redisAddress)
     }
 
-    private createQueue(name: string): void {
+    private createQueue(name: string): Queue {
         if (this.queues.has(name)) {
-            return
+            return this.queues.get(name)
         }
-        this.queues.set(name, new Queue(name))
-        this.queuesEvents.set(name, new QueueEvents(name))
+
+        const queue = new Queue(name)
+        this.queues.set(name, queue)
+
+        const events = new QueueEvents(name)
+        this.queuesEvents.set(name, events)
+
+        events.on('completed', (job: Job) => this.responses$.next(job))
+
+        return queue
     }
 
-    // public addJob()
+    public addJob(project: string, type: string, payload?: any) {
+        const queue = this.createQueue(project)
+
+        // this
+    }
 
     public createWorker(name: string, callback: GenericFunction): void {
         this.worker = new Worker(name, callback, { connection: this.redis })
