69d606272c180eb44da5f3a87208fe70837dbc96
Alykam Budzaki <alykam@meucrediario.com.br>
2021-06-18 14:21:09 -0300

    export dtos

---
+++ apps/common/dtos/hero.ts
@@ -0,0 +1,8 @@
+export class Hero {
+    name: string
+    hp = 100
+
+    constructor(name: string) {
+        this.name = name
+    }
+}
---
+++ apps/common/dtos/monster.ts
@@ -0,0 +1,8 @@
+export class Monster {
+    type: string
+    hp = 100
+
+    constructor(type: string) {
+        this.type = type
+    }
+}
